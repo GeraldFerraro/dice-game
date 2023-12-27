e4393f5b981334c71359cacab3f7a24ad9213de2
Gerald Ferraro <122627229+GeraldFerraro@users.noreply.github.com>
2023-12-27 12:28:26 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+var randomNumber1 = Math.floor(Math.random()*6) + 1;
+var randomDiceImageSource1 = "images/dice" + randomNumber1 + ".png";
+var image1 = document.querySelector(".img1");
+image1.setAttribute("src", randomDiceImageSource1);
+
+var randomNumber2 = Math.floor(Math.random()*6) + 1;
+var randomDiceImageSource2 = "images/dice" + randomNumber2 + ".png";
+var image2 = document.querySelector(".img2");
+image2.setAttribute("src", randomDiceImageSource2);
+
+if(randomNumber1 === randomNumber2) {
+    document.querySelector("h1").innerHTML = "Draw!";
+} else if(randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 Wins!"
+} else {
+    document.querySelector("h1").innerHTML = "Player 2 Wins!"
+}
